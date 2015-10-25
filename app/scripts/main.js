0e0bd4d32b596750906273799a8bce2cc28a65b4
Gheorgheoiu Nicolae <nicolae.gheorgheoiu@businessmail.avelon-cetex.com>
2015-10-25 09:50:24 +0200

    Added the fonts to production version.

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -7,14 +7,6 @@ $(document).ready(function(){
   });
 
 
-  var options = [
-      {selector: '#main-nav', offset: 50, callback: 'Materialize.toast("This is our ScrollFire Demo!", 1500 )' },
-      {selector: '#main-nav', offset: 205, callback: 'Materialize.toast("Please continue scrolling!", 1500 )' },
-      {selector: '#main-nav', offset: 400, callback: 'Materialize.showStaggeredList("#staggered-test")' },
-      {selector: '#image-test', offset: 5000, callback: 'Materialize.fadeInImage("#image-test")' }
-  ];
-  Materialize.scrollFire(options);
-
 
   $('.materialboxed').materialbox();
 
